482bee6d1b5683c4f63b8cdbfc21ff6e6dc7e5fd
CODIGO2022-B <141700983+CODIGO2022-B@users.noreply.github.com>
2023-11-17 21:00:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
-document.addEventListener('mousemove', function (e) {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    document.getElementById('fondo').style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+document.addEventListener('mousemove', function(e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  document.getElementById('fondo').style.transform = `translate(${xAxis}px, ${yAxis}px)`;
 });
 
 function irAPagina() {
-    window.location.href = 'otra_pagina.html';
+  window.location.href = 'otra_pagina.html';
 }
 
-// Añade esta función para mantener el botón visible
-window.addEventListener('scroll', function() {
-    document.getElementById('inicio').style.bottom = '20px'; // Ajusta según sea necesario
-});
-// ... Tu código existente ...
-
-let audioEnabled = true; // Variable para rastrear si el audio está habilitado
+let audioEnabled = true;
 
 function toggleAudio() {
-    const audioIcon = document.getElementById('audio-icon');
-    const audio = document.getElementById('mi-audio'); // Reemplaza 'mi-audio' con el ID de tu elemento de audio
+  const audioIcon = document.getElementById('audio-icon');
+  const audio = document.getElementById('mi-audio');
 
-    audioEnabled = !audioEnabled; // Invierte el estado del audio
+  audioEnabled = !audioEnabled;
 
-    if (audioEnabled) {
-        audioIcon.querySelector('[fill="#808080"]').style.display = 'none';
-        audioIcon.querySelector('[fill="#00FF00"]').style.display = 'block';
-        audioIcon.classList.remove('muted');
-        audio.volume = 1; // Restaura el volumen al máximo
-    } else {
-        audioIcon.querySelector('[fill="#00FF00"]').style.display = 'none';
-        audioIcon.querySelector('[fill="#808080"]').style.display = 'block';
-        audioIcon.classList.add('muted');
-        audio.volume = 0; // Establece el volumen en 0 (muted)
-    }
-} 
+  if (audioEnabled) {
+    audioIcon.querySelector('[fill="#808080"]').style.display = 'none';
+    audioIcon.querySelector('[fill="#00FF00"]').style.display = 'block';
+    audioIcon.classList.remove('muted');
+    audio.volume = 1;
+  } else {
+    audioIcon.querySelector('[fill="#00FF00"]').style.display = 'none';
+    audioIcon.querySelector('[fill="#808080"]').style.display = 'block';
+    audioIcon.classList.add('muted');
+    audio.volume = 0;
+  }
+}
+
+function playAudio() {
+  const audio = document.getElementById('a.mp3');
+
+  if (audio.paused) {
+    audio.play();
+    document.getElementById('reproducir').style.display = 'none';
+  } else {
+    audio.pause();
+    document.getElementById('reproducir').style.display = 'block';
+  }
+}
